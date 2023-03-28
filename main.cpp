be397c9e7677e5def997180bbf6689a5fe8c22c8
Alexandre Souto <soutoinf@gmail.com>
2023-03-28 14:31:22 -0300

    init

--- main.cpp
+++ main.cpp
@@ -1,55 +1,75 @@
 #include "Includes.hpp"
 #include <algorithm>
 
+void Processamento(string nameIn_, string nameOut1_, string nameOut2_);
 
 int main() {
 
     
+    while(1)
+    {
+        string nameIn, nameOut1, nameOut2;
+        cout << "Digite o nome do arquivo de o entrada Ex.[nomeArquivo.txt].\n";
+        cin >> nameIn;
+        cout << "Digite o nome do arquivo de saida Ex.[nomeArquivo.txt].\n";
+        cin >> nameOut1;
+        nameOut2 = nameOut2 + "_2.txt";
+        Processamento(nameIn,nameOut1,nameOut2);
+
+        return 0;
+    }
 
 
-    string inputFileName = text_ctrl.GetValue()
-    string outputFileName = "1177_clean1.txt";
-    string outputFileName2 = "1177_clean2.txt";
+}
+
+void Processamento(string nameIn_, string nameOut1_, string nameOut2_)
+{
+
+    string inputFileName = nameIn_;
+    string outputFileName = nameOut1_;
+    string outputFileName2 = nameOut2_;
 
     ifstream inputFile(inputFileName);
-    ofstream outputFile(outputFileName);
-    ofstream outputFile2(outputFileName2);
+        ofstream outputFile(outputFileName);
+        ofstream outputFile2(outputFileName2);
 
-    size_t pos = 0;
-    string line;
-    string delimiter = "GPS:";
-    string delimiter2 = ": ";
-    int contador = 0;
+        size_t pos = 0;
+        string line;
+        string delimiter = "GPS:";
+        string delimiter2 = ": ";
+        int contador = 0;
 
-    if (inputFile.is_open() && outputFile.is_open()) {
-        while (getline(inputFile, line)) {
-            if (line.find(delimiter) != string::npos) {
-                contador++;
-                while ((pos = line.find(delimiter2)) != string::npos)
-                {
-                    line.erase(0, pos + delimiter2.length());
-                    line = line;
-                }
-                if (contador > 25000)
-                {
-                    outputFile2 << line << endl;
-                }
-                else 
-                {
-                    outputFile << line << endl;
-                }
-                
-            }    
+        if (inputFile.is_open() && outputFile.is_open()) {
+            while (getline(inputFile, line)) {
+                if (line.find(delimiter) != string::npos) {
+                    contador++;
+                    while ((pos = line.find(delimiter2)) != string::npos)
+                    {
+                        line.erase(0, pos + delimiter2.length());
+                        line = line;
+                    }
+                    if (contador > 25000)
+                    {
+                        outputFile2 << line << endl;
+                    }
+                    else 
+                    {
+                        outputFile << line << endl;
+                    }
+                    
+                }    
+            }
+            inputFile.close();
+            outputFile2.close();
+
+            cout << "Deu bom!\n";
+        } else {
+            cout << "Pau pra abrir essa porra.\n";
         }
-        inputFile.close();
-        outputFile2.close();
 
-        cout << "Deu bom!\n";
-    } else {
-        cout << "Pau pra abrir essa porra.\n";
-    }
-    return 0;
+
 }
+
 
 
 
